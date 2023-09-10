7ff25b4d4a743002b319f89d0f80b3399d9f9b98
rifat <rifat509924@gmail.com>
2023-09-10 17:31:18 -0500

    Add project files.

---
+++ AdvancedDatabaseAndORM_FinalAssesment/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AdvancedDatabaseAndORM_FinalAssesment.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
